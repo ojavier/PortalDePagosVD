fce705fe7e3d20ff2f9c3f5f724c234d0d107990
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2024-03-17 14:48:57 -0600

    Delete Portal de Pagos directory
    Delete the directory related to Portal de Pagos until it's ready for release.

--- Portal de Pagos/controllers/main.controller.js
+++
@@ -1,5 +0,0 @@
-exports.get_root = (request, response, next) => {
-    response.render('home', {
-        pagePrimaryTitle: 'Portal de Gestión de Pagos',
-    });
-};
--- Portal de Pagos/routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const mainRouteController = require('../controllers/main.controller');
-
-router.get('/', mainRouteController.get_root);
-
-module.exports = router;
