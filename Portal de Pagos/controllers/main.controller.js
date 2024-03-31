b22353da2c5a0f4fdbc7d8cfe7fbefd59adc0518
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2024-03-30 22:27:40 -0600

    Update main.controller.js

--- Portal de Pagos/controllers/main.controller.js
+++ Portal de Pagos/controllers/main.controller.js
@@ -1,5 +1,5 @@
 exports.get_root = (request, response, next) => {
-    response.render('payment-methods', {
+    response.render('home2', {
         pagePrimaryTitle: 'Portal de Gestión de Pagos',
     });
 };
