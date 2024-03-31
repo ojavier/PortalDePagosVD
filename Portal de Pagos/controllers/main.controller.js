7a249f08c8586ba434b40fabe6d593bb4ed77598
ELECTRIC80008STORM <63619664+ELECTRIC80008STORM@users.noreply.github.com>
2024-03-31 11:46:36 -0600

    Revert "Revert "Add the view where the admin can modify the values of credits and view past credit values""

--- Portal de Pagos/controllers/main.controller.js
+++ Portal de Pagos/controllers/main.controller.js
@@ -3,3 +3,15 @@ exports.get_root = (request, response, next) => {
         pagePrimaryTitle: 'Portal de Gestión de Pagos',
     });
 };
+
+exports.get_creditos = (request, response, next) => {
+    response.render('creditos', {
+        pagePrimaryTitle: 'Créditos',
+    });
+};
+
+exports.get_configuracion = (request, response, next) => {
+    response.render('configuracion', {
+        pagePrimaryTitle: 'Configuración',
+    });
+};
