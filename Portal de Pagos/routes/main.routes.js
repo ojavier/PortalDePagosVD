80c4ede8172239c475d771539523d3fb278a5115
ojavier <ojvilleda6@gmail.com>
2024-04-18 17:49:21 -0600

    is auth

--- Portal de Pagos/routes/main.routes.js
+++ Portal de Pagos/routes/main.routes.js
@@ -1,37 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../util/is-auth');
 
 const mainController = require('../controllers/main.controller');
 
 router.get('/', mainController.get_root);
 
-router.get('/home', mainController.get_login);
+router.get('/home', isAuth, mainController.get_login);
 
-router.get('/home2', mainController.get_home);
+router.get('/home2', isAuth, mainController.get_home);
 
-router.get('/paymethod', mainController.get_paymethod);
+router.get('/paymethod', isAuth, mainController.get_paymethod);
 
-router.get('/payplan', mainController.get_payplan);
+router.get('/payplan', isAuth, mainController.get_payplan);
 
-router.get('/profile', mainController.get_profile);
+router.get('/profile', isAuth, mainController.get_profile);
 
-router.get('/reportes', mainController.get_reportes);
+router.get('/reportes', isAuth, mainController.get_reportes);
 
-router.get('/creditos', mainController.get_creditos);
+router.get('/creditos', isAuth, mainController.get_creditos);
 
-router.post('/creditos', mainController.post_cicloescolar);
+router.post('/creditos', isAuth, mainController.post_cicloescolar);
 
-router.get('/configuracion', mainController.get_configuracion);
+router.get('/configuracion', isAuth, mainController.get_configuracion);
 
-router.post('/configuracion', mainController.post_configuracion);
+router.post('/configuracion', isAuth, mainController.post_configuracion);
 
-router.get('/pagos', mainController.get_pagos);
+router.get('/pagos', isAuth, mainController.get_pagos);
 
-router.post('/pagos', mainController.post_Forms);
+router.post('/pagos', isAuth, mainController.post_Forms);
 
-router.get('/importar', mainController.get_importar);
+router.get('/importar', isAuth, mainController.get_importar);
 
-router.post('/importar', mainController.post_importar);
-
+router.post('/importar', isAuth, mainController.post_importar);
 
 module.exports = router;
--- Portal de Pagos/routes/users.routes.js
+++ Portal de Pagos/routes/users.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
 const usersController = require('../controllers/users.controller');
 
 router.get('/login', usersController.get_login);
 router.post('/login', usersController.post_login);
+
+router.get('/logout', usersController.get_logout);
 
 module.exports = router;
