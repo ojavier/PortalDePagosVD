7eecbb81506f0b6d3586ee194405792cb804ab33
Rommel <rommtoledo@gmail.com>
2024-04-19 05:42:11 -0600

    Fix login and extract privileges

--- Portal de Pagos/controllers/users.controller.js
+++ Portal de Pagos/controllers/users.controller.js
@@ -10,21 +10,38 @@ exports.get_login = (request, response, next) => {
 };
 
 exports.post_login = (request, response, next) => {
-    console.log(request.body.Email);
+    console.log("Datos recibidos: ", request.body.Email, request.body.Password);
     Usuario.fetchOne(request.body.Email)
-        .then(([usuarios, fieldData]) => {
-            console.log(usuarios);
-            if (usuarios) {
-                const usuario = usuarios[0];
-                // Establecer isLoggedIn en true
-                request.session.isLoggedIn = true;
-                response.redirect('/home');
+        .then(([usuario]) => {  // Aquí cambiamos 'usuarios' a 'usuario' para reflejar que es un objeto único, no un array
+            console.log("Usuario encontrado: ", usuario);
+            if (usuario) {  // Aquí ya no necesitas verificar 'usuario.length > 0'
+                if (usuario.Password === request.body.Password) {
+                    console.log('Valid Password');
+                    request.session.isLoggedIn = true;
+                    Usuario.getPrivilegios(request.body.Email)
+                        .then(([permits, fieldData]) => {
+                            request.session.permisos = permits.map(permiso => permiso.NombrePrivilegio);
+                            response.redirect('/home');
+                        })
+                        .catch((error) => {
+                            console.error('Error al extraer privilegios', error);
+                            response.redirect('/users/login');
+                        });
+                } else {
+                    console.log('Contraseña incorrecta');
+                    request.session.error = "Usuario y/o contraseña incorrectos";
+                    response.redirect('/users/login');
+                }
             } else {
+                console.log('No se encontró el usuario');
                 request.session.error = "Usuario y/o contraseña incorrectos";
                 response.redirect('/users/login');
             }
         })
-        .catch((error) => {console.log(error);});
+        .catch((error) => {
+            console.error('Error en fetchOne', error);
+            response.redirect('/users/login');
+        });
 };
 
 
