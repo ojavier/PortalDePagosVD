83ed34c62a6c9751c06cf4579e85277e9ca01978
Rommel <rommtoledo@gmail.com>
2024-04-25 23:13:58 -0600

    Active the user profile button in the navbar

--- Portal de Pagos/routes/users.routes.js
+++ Portal de Pagos/routes/users.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+const isAuth = require('../util/is-auth');
 
 const usersController = require('../controllers/users.controller');
 
-router.get('/profile', usersController.get_profile);
+router.get('/profile', isAuth, usersController.get_profile);
 router.get('/login', usersController.get_login);
 router.post('/login', usersController.post_login);
 router.get('/logout', usersController.get_logout);
