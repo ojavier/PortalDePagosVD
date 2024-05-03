f615d83c075c822e806468413d6df3917fa40188
ojavier <ojvilleda6@gmail.com>
2024-05-03 04:25:53 -0600

    generacion correcta del url

--- Portal de Pagos/controllers/bank.controller.js
+++ Portal de Pagos/controllers/bank.controller.js
@@ -18,119 +18,127 @@ exports.generarURL = (request, response) => {
       if (solicitudes.length > 0) {
         const solicitud = solicitudes[0]; // Suponiendo que solo obtienes una solicitud
         const cantidad = solicitud.cantidad;
-
+        
         // Crear la cadena XML con XML Builder
-        const xmlData = builder.create('P')
-          .ele('business')
-            .ele('id_company', 'SNBX')
-            .up()
-            .ele('id_branch', '01SNBXBRNCH')
-            .up()
-            .ele('user', 'SNBXUSR0123')
-            .up()
-            .ele('pwd', 'SECRETO')
-          .up()
-          .ele('nb_fpago', 'COD')
-          .up()
-          .ele('url')
-            .ele('reference', reference)
-            .up()
-            .ele('amount', cantidad.toString())
-            .up()
-            .ele('moneda', 'MXN')
-            .up()
-            .ele('canal', 'W')
-            .up()
-            .ele('omitir_notif_default', '1')
-            .up()
-            .ele('promociones', 'C,3,6')
-            .up()
-            .ele('id_promotion', 'SNBX12345678')
-            .up()
-            .ele('st_correo', '1')
-            .up()
-            .ele('fh_vigencia', '09/09/2021')
-            .up()
-            .ele('mail_cliente', solicitud.email.toString()) // Usar el email de la solicitud
-            .up()
-            .ele('data3ds')
-              .ele('ml', 'nospam@gmail.com')
-            .up()
-            .ele('datos_adicionales')
-              .ele('data', {'id': '1', 'display': 'true'})
-                .ele('label', 'Talla')
-                .up()
-                .ele('value', 'Grande')
-              .up()
-              .ele('data', {'id': '2', 'display': 'false'})
-                .ele('label', 'Color')
-                .up()
-                .ele('value', 'Azul')
-              .up()
-            .up()
-          .ele('nb_fpago', 'COD')
-          .up()
-          .ele('dom', '1')
-          .up()
-          .ele('version', 'IntegraWPP')
-          .up()
+      const xmlData = builder.create('P')
+      .ele('business')
+        .ele('id_company', 'SNBX')
         .up()
-        .end({ pretty: true });
+        .ele('id_branch', '01SNBXBRNCH')
+        .up()
+        .ele('user', 'SNBXUSR0123')
+        .up()
+        .ele('pwd', 'SECRETO')
+      .up() // Cierre del elemento <business>
+    .up() // Cierre del elemento <P>
+    .ele('nb_fpago', 'COD') // Declaración de nb_fpago fuera de business
+      .up() // Cierre de nb_fpago
+    .ele('url') // Inicio del elemento <url>
+      .ele('reference', reference)
+      .up()
+      .ele('amount', cantidad.toString())
+      .up()
+      .ele('moneda', 'MXN')
+      .up()
+      .ele('canal', 'W')
+      .up()
+      .ele('omitir_notif_default', '1')
+      .up()
+      .ele('id_promotion', 'SNBX12345678')
+      .up()
+      .ele('st_correo', '1')
+      .up()
+      .ele('fh_vigencia', '09/09/2021')
+      .up()
+      .ele('mail_cliente', solicitud.email.toString()) // Usar el email de la solicitud
+      .up()
+      .ele('data3ds')
+        .ele('ml', 'nospam@gmail.com')
+        .up()
+        .ele('cl', '5515009020')
+        .up()
+        .ele('dir', 'Calle y número exterior')
+        .up()
+        .ele('cd', 'Ciudad')
+        .up()
+        .ele('est', 'CX')
+        .up()
+        .ele('cp', '1234567890')
+        .up()
+        .ele('idc', '484')
+        .up() // Cerrar data3ds
+      .up() // Cerrar url
+    .ele('version', 'IntegraWPP') // <version>IntegraWPP</version>
+    .up() // Cerrar la etiqueta version
+  .end({ pretty: true });
+    
 
         // Imprimir la cadena XML
         console.log('Cadena XML:', xmlData.toString());
 
-        // Enviar el XML como parte de la respuesta HTTP
-        response.status(200).send(xmlData.toString());
+        // Definir algoritmo, clave e iv
+      const algorithm = 'aes-128-cbc';
+      const key = Buffer.from('5DCC67393750523CD165F17E1EFADD21', 'hex');
+      const iv = crypto.randomBytes(16);
 
-        // Función para desencriptar
-        const desencriptar = (encryptedData, key, iv) => {
-          // Convertir la clave de hexadecimal a Buffer
-          const keyBuffer = Buffer.from(key, 'hex');
-          
-          const decipher = crypto.createDecipheriv(algorithm, keyBuffer, iv);
-          let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
-          decryptedData += decipher.final('utf-8');
-          return decryptedData;
-        };
+      // Función para encriptar
+      const encriptar = (data, key, iv) => {
+        const cipher = crypto.createCipheriv(algorithm, key, iv);
+        let encryptedData = cipher.update(data, 'utf-8', 'hex');
+        encryptedData += cipher.final('hex');
+        return encryptedData;
+      };
 
-        // Encriptar la cadena XML
-        const encryptedXmlData = xmlData.toString();
+      // Encriptar la cadena XML
+      const encryptedXmlData = encriptar(xmlData.toString(), key, iv);
+      console.log('Cadena XML encriptada:', encryptedXmlData);
 
-        // Ejemplo de desencriptación
-        const decryptedXmlData = desencriptar(encryptedXmlData, key, iv);
-        console.log('Cadena XML desencriptada:', decryptedXmlData);
 
         // Generar URL y enviar solicitud POST
         const urlData = `xml=${encodeURIComponent(encryptedXmlData)}`;
 
-        axios.post('https://sandboxpo.mit.com.mx/gen', urlData, {
+        const axios = require('axios');
+
+        const originalString = "xml=<pgs><data0>SNDBX123</data0><data>Cadena Cifrada</data></pgs>";
+        const data = encodeURIComponent(originalString);
+
+        axios.post('https://sandboxpo.mit.com.mx/gen', data, {
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
           }
         })
         .then(response => {
           console.log('Respuesta del servidor:', response.data);
-          // Aquí podrías enviar la respuesta al cliente si es necesario
-
-          // Redirigir al cliente a la URL generada
-          console.log('Redirigiendo al cliente a la URL:', response.data.url);
-          response.redirect(response.data.url); // Suponiendo que la respuesta del servidor contiene la URL generada
         })
         .catch(error => {
           console.error('Error al enviar solicitud:', error);
           // Manejo de errores y respuesta al cliente si es necesario
-          response.status(500).send('Error en el servidor');
         });
+
+
       } else {
         console.error('No se encontró ninguna solicitud de pago.');
         response.status(404).send('No se encontró ninguna solicitud de pago.');
       }
     })
-    .catch(error => {
-      console.error('Error al obtener la solicitud de pago:', error);
-      response.status(500).send('Error en el servidor');
-    });
+
+    // Función para desencriptar
+    const desencriptar = (encryptedData, key, iv) => {
+      // Convertir la clave de hexadecimal a Buffer
+      const keyBuffer = Buffer.from(key, 'hex');
+          
+      const decipher = crypto.createDecipheriv(algorithm, keyBuffer, iv);
+      let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+      decryptedData += decipher.final('utf-8');
+      return decryptedData;
+    };
+
+    // Ejemplo de desencriptación
+    const decryptedXmlData = desencriptar(encryptedXmlData, key, iv);
+    console.log('Cadena XML desencriptada:', decryptedXmlData);
+
+    // Enviar el XML como parte de la respuesta HTTP
+    response.status(200).send(xmlData.toString());
 };
 
 // Función para manejar la respuesta del servidor
